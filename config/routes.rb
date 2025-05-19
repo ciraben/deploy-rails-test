91c3c631829b956a7ca79305d4d1b4d60d545760
Tom Van Manen <tom.on.github@gmail.com>
2025-05-19 13:19:25 -0700

    Route product requests and generate a controller
    - Runs bin/rails generate controller Products index
    - Edits the routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  # get "/products", to: "products#index"
+  #
+  # get "/products/new", to: "products#new"
+  # post "/products", to: "products#create"
+  #
+  # get "/products/:id", to: "products#show"
+  #
+  # get "/products/:id/edit", to: "products#edit"
+  # patch "/products/:id", to: "products#update"
+  # put "/products/:id", to: "products#update"
+  #
+  # delete "/products/:id", to: "products#destroy"
+  resources :products
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
